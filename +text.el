d01bf83e02172b6d138e30a0cf40fecb2faf4ab2
“mk0417” <pengdata1@gmail.com>
2020-11-03 20:10:28 +0000

    clean code

--- +text.el
+++ +text.el
@@ -47,17 +47,17 @@
         (propertize "Presentation mode ON" 'face 'success)
         org-tree-slide-deactivate-message
         (propertize "Presentation mode OFF" 'face 'error))
-  (setq org-roam-server-host "127.0.0.1"
-        org-roam-server-port 8080
-        org-roam-server-authenticate nil
-        org-roam-server-export-inline-images t
-        org-roam-server-serve-files nil
-        org-roam-server-served-file-extensions '("pdf" "mp4" "ogv")
-        org-roam-server-network-poll t
-        org-roam-server-network-arrows nil
-        org-roam-server-network-label-truncate t
-        org-roam-server-network-label-truncate-length 60
-        org-roam-server-network-label-wrap-length 20)
+  ;; (setq org-roam-server-host "127.0.0.1"
+  ;;       org-roam-server-port 8080
+  ;;       org-roam-server-authenticate nil
+  ;;       org-roam-server-export-inline-images t
+  ;;       org-roam-server-serve-files nil
+  ;;       org-roam-server-served-file-extensions '("pdf" "mp4" "ogv")
+  ;;       org-roam-server-network-poll t
+  ;;       org-roam-server-network-arrows nil
+  ;;       org-roam-server-network-label-truncate t
+  ;;       org-roam-server-network-label-truncate-length 60
+  ;;       org-roam-server-network-label-wrap-length 20)
   ;; overwrite default templates
   ;; does not work if move it out of after! org
   (setq org-capture-templates
