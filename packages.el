bcfca96c4b8b99348995e6b04ea733018bc87416
“mk0417” <pengdata1@gmail.com>
2021-02-05 12:32:26 +0000

    symbol-overlay

--- packages.el
+++ packages.el
@@ -22,6 +22,7 @@
 (package! srcery-theme)
 (package! modus-themes)
 (package! code-cells)
+(package! symbol-overlay)
 
 ;; To install a package directly from a remote git repo, you must specify a
 ;; `:recipe'. You'll find documentation on what `:recipe' accepts here:
