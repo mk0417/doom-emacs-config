9daa18d1bb669146206643a3d9e70fb8dbf7987b
“mk0417” <pengdata1@gmail.com>
2021-02-18 11:16:25 +0000

    eacl

--- packages.el
+++ packages.el
@@ -24,6 +24,7 @@
 (package! code-cells)
 (package! symbol-overlay)
 (package! define-word)
+(package! eacl)
 
 ;; To install a package directly from a remote git repo, you must specify a
 ;; `:recipe'. You'll find documentation on what `:recipe' accepts here:
