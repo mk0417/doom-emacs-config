00c81c698e7282d9fe449e4a46d0d21dc9e12267
“mk0417” <pengdata1@gmail.com>
2021-03-07 22:58:01 +0000

    remove childframe

--- init.el
+++ init.el
@@ -20,11 +20,10 @@
        ;;layout            ; auie,ctsrnm is the superior home row
 
        :completion
-       (company +childframe)           ; the ultimate code completion backend
+       company           ; the ultimate code completion backend
        ;;helm              ; the *other* search engine for love and life
        ;;ido               ; the other *other* search engine...
-       (ivy +prescient
-            +childframe)              ; a search engine for love and life
+       (ivy +prescient)              ; a search engine for love and life
 
        :ui
        ;; deft              ; notational velocity for Emacs
